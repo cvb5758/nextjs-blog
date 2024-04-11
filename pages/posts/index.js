1759f4668508660f8cc97dd9ce161ec9c4f63d65
waterfall <cvb575859@gmail.com>
2024-04-12 00:50:06 +0900

    [setting] page setting
    필요한 page setting

---
+++ pages/posts/index.js
@@ -0,0 +1,11 @@
+export default function AllPostsPage() {
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+      </ul>
+    </div>
+  );
+}
