b3a8f272fef53d690813fc95431ef259d17f30fa
waterfall <cvb575859@gmail.com>
2024-04-14 13:31:14 +0900

    feat: Hero component
    main page Hero component 구현

---
+++ components/home-page/hero.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import classes from './hero.module.css';
+
+export default function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/sumin.jpg"
+          alt="An image showing Sumin"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I am Sumin</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import FeaturedPosts from '@/components/home-page/featured-posts';
+import Hero from '@/components/home-page/hero';
+
 export default function HomePage() {
   return (
     <>
