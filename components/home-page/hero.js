d150f4b2053f6f33856934a2bb6a22a308c8364c
waterfall <cvb575859@gmail.com>
2024-05-03 13:04:44 +0900

    fix: 잘못된 blurDataUrl 제거

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,9 +1,6 @@
 import Image from 'next/image';
 
 export default function Hero() {
-  const blurDataUrl =
-    'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCABkAGQDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP090tLS1NXW19jZ2uLk5+Pf/2Q==';
-
   return (
     <section
       className="w-full mx-auto flex 
@@ -17,7 +14,6 @@ export default function Hero() {
           width={400}
           height={400}
           className="shadow-lg rounded-md"
-          placeholder={blurDataUrl}
         />
       </figure>
     </section>
