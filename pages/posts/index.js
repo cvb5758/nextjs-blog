9414f9379be831b1ee2deb810f61a83695a7980e
waterfall <cvb575859@gmail.com>
2024-05-03 12:47:17 +0900

    feat: 이미지 base64 인코딩 구현
    이미지 플레이스홀더에 넣기 위한
    이미지 base64 인코딩 기능 구현

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,4 +1,5 @@
 import AllPost from '@/components/posts/all-posts';
+import { encodeImageToBlurDataURL } from '@/lib/image-utils';
 import { getAllPosts } from '@/lib/posts-utils';
 
 export default function AllPostsPage(props) {
@@ -9,11 +10,23 @@ export default function AllPostsPage(props) {
   );
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
   const posts = getAllPosts();
+
+  const postsWithImages = await Promise.all(
+    posts.map(async (post) => {
+      const blurDataURL = await encodeImageToBlurDataURL(
+        `/public/images/posts/${post.slug}/${post.image}`
+      );
+      return {
+        ...post,
+        blurDataURL,
+      };
+    })
+  );
   return {
     props: {
-      posts: posts,
+      posts: postsWithImages,
     },
   };
 }
