8156f479047de763c54c850e5decd2398c020fae
waterfall <cvb575859@gmail.com>
2024-05-04 16:48:56 +0900

    style: post 페이지 반응형 수정
    post 페이지에서 width 640px 이하로 내려
    갔을때 생기던 스타일 오류 수정

--- components/posts/posts-grid.js
+++ components/posts/posts-grid.js
@@ -4,7 +4,7 @@ import classes from './posts-grid.module.css';
 export default function PostsGrid(props) {
   const { posts } = props;
   return (
-    <ul className="grid grid-cols-1 md:grid-cols-2 gap-y-12 gap-x-6">
+    <ul className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-8 my-8">
       {posts.map((post) => (
         <PostsItem key={post.slug} post={post} />
       ))}
--- components/posts/posts-item.js
+++ components/posts/posts-item.js
@@ -14,20 +14,25 @@ export default function PostsItem(props) {
   const linkPath = `/posts/${slug}`;
 
   return (
-    <li key={slug} className="border border-gray-400 rounded-md p-4">
+    <li
+      key={slug}
+      className="border border-gray-400 rounded-md p-4 transition duration-300 ease-in-out transform hover:scale-105
+      shadow-md hover:shadow-lg hover:border-gray-500
+      "
+    >
       <Link href={linkPath}>
-        <div className="flex flex-col gap-4 w-full h-full cursor-pointer">
+        <div className="flex flex-col  gap-4 w-full h-full cursor-pointer items-center">
           <Image
             src={imagePath}
             alt={title}
             width={320}
             height={320}
-            className="rounded-md h-40 shadow-md transition duration-300 ease-in-out transform hover:scale-110"
+            className="rounded-md h-40 shadow-md w-full"
             placeholder={blurDataURL}
           />
 
           <div className="flex flex-col justify-end items-start text-gray-700">
-            <h className="text-xl font-bold">{title}</h>
+            <h1 className="text-xl font-bold">{title}</h1>
             <time
               className="
             text-gray-500 font-semibold text-sm mt-2"
